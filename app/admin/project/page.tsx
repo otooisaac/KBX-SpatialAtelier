f472bcb6364e68a5b2c4f993d9fae41ea0799347
otooisaac <otooisaackb2003@gmail.com>
2026-09-15 16:52:20 +0000

    gdhgh

--- app/admin/project/page.tsx
+++ app/admin/project/page.tsx
@@ -967,7 +967,7 @@ export default function Home() {
             </div>
 
             <Link
-              href="/admin/project"
+              href="/app/admin/project"
               aria-label="Website developer"
               className="mt-4 block text-center text-[9px] text-black/25 transition hover:text-black/40"
             >
